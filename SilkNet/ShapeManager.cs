56d93ff7e15e7b4be106a350d79d9cb4c8ba71af
Kael Foxwell <kaefoxewe@gmail.com>
2023-09-10 17:40:26 +1000

    abstraction

---
+++ SilkNet/ShapeManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkNet
+{
+    internal class ShapeManager
+    {
+        public static ShapeManager Instance = new ShapeManager();
+
+        private List<ITriangleShape> _shapes = new();
+
+        public void AddTriangleShape(ITriangleShape shape) => _shapes.Add(shape);
+
+        public void GetCombinedMesh(List<float> vertices, List<uint> indices)
+        {
+            foreach (var shape in _shapes)
+            {
+                shape.CombineMesh(vertices, indices);
+            }
+        }
+    }
+}
