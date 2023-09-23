c7a4cba7e1e0a8476bb98616a41fd5ba52a2aa11
Kael Foxwell <kaefoxewe@gmail.com>
2023-09-24 09:49:20 +1000

    basic down

---
+++ FNA_Learning/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace FNA_Learning
+{
+    internal class FNAGame : Game
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // following this https://github.com/FNA-XNA/FNA/wiki/2b:-FNA-From-Scratch-Tutorial#your-first-game
+
+            using (FNAGame game = new FNAGame())
+            {
+                game.Run();
+            };
+        }
+
+        private FNAGame()
+        {
+            GraphicsDeviceManager gManager = new GraphicsDeviceManager(this);
+
+            gManager.PreferredBackBufferWidth = 1000;
+            gManager.PreferredBackBufferHeight = 800;
+            gManager.IsFullScreen = false;
+
+            gManager.ApplyChanges();
+
+            Window.IsBorderlessEXT = true;
+        }
+
+        protected override void Initialize()
+        {
+            /* This is a nice place to start up the engine, after
+             * loading configuration stuff in the constructor
+             */
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Load textures, sounds, and so on in here...
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            // Clean up after yourself!
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // Run game logic in here. Do NOT render anything here!
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            // Render stuff in here. Do NOT run game logic in here!
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+    }
+}
